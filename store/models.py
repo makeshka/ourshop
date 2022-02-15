a6ffe2a06e779dae3dd0ef9b9d036369f18b5ba0
makeshka <200103172@stu.sdu.edu.kz>
2022-02-15 23:05:29 +0600

    hello

--- store/models.py
+++ store/models.py
@@ -1,3 +1,38 @@
+from tabnanny import verbose
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# okey blyat
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    producer = models.ForeignKey(User, related_name='product_creater', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200, default='unknown')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to = 'image/')
+    slug = models.SlugField(max_length=200)
+    price = models.DecimalField(max_digits=4, decimal_places=3)
+    in_stock = models.BooleanField(default=True)
+    in_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updaded = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'products'
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
