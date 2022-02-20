4e0dda0cd35a71d2fa5ea9ec600b6b45c406ee32
Seitmurat <200103125@stu.sdu.edu.kz>
2022-02-21 04:50:09 +0600

    html

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'store'
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
+    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
     
 ]
