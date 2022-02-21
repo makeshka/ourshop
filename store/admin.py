5e845951431759e84c769964595875d5786767f8
makeshka <200103172@stu.sdu.edu.kz>
2022-02-21 12:36:09 +0600

    part 1 end

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 import imp
+
 from django.contrib import admin
 
 # Register your models here.
 from .models import Category, Product
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- store/models.py
+++ store/models.py
@@ -1,7 +1,10 @@
+import imp
 from tabnanny import verbose
 from turtle import title
-from django.db import models
+
 from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 # okey blyat
@@ -12,9 +15,13 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
-    
+
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+    
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
@@ -24,7 +31,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to = 'image/')
     slug = models.SlugField(max_length=200)
-    price = models.DecimalField(max_digits=4, decimal_places=3)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     in_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -36,3 +43,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+    
