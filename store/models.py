62af5311c9645e4fdcd56e15c9cc31b99c48a8cd
Seitmurat <200103125@stu.sdu.edu.kz>
2022-02-24 10:47:32 +0600

    Merge branch 'master' of https://github.com/makeshka/ourshop

--- store/models.py
+++ store/models.py
@@ -32,7 +32,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to = 'image/')
     slug = models.SlugField(max_length=200)
-    price = models.DecimalField(max_digits=5)
+    price = models.DecimalField(max_digits=5, decimal_places=0)
     in_stock = models.BooleanField(default=True)
     in_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
