d1ac7832f6f9ce9240cc7ce89a58bbd8aab09481
Seitmurat <200103125@stu.sdu.edu.kz>
2022-05-17 01:38:50 +0600

    Design changing

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
     path('basket/', include('basket.urls', namespace='basket')),
     path('payment/', include('payment.urls', namespace='payment')),
